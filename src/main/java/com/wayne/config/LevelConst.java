aa3bca5e828eabd7b683acdca1cc0cc6d6601b6b
gutao <542779063@qq.com>
2017-03-27 11:39:34 +0800

    service 实现1

--- src/main/java/com/wayne/config/LevelConst.java
+++ src/main/java/com/wayne/config/LevelConst.java
@@ -4,14 +4,20 @@ package com.wayne.config;
  * @Author TAO
  * @Date 2017/3/27 10:54
  */
-public class LevelEnum {
+public class LevelConst {
+
+    private LevelConst(){}
 
     //level 用户等级
+    public static final int REFRESH_THRESHOLD = 30;
+    public static final int OLD_THRESHOLD = 100;
+    public static final int TEACHER_THRESHOLD = 200;
+    public static final int DIRECTOR_THRESHOLD = 350;
+    public static final int PRESIDENT_THRESHOLD = 700;
 
-    int REFRESH_THRESHOLD = 30;
-    int OLD_THRESHOLD = 100;
-    int TEACHER_THRESHOLD = 200;
-    int DIRECTOR_THRESHOLD = 350;
-    int PRESIDENT_THRESHOLD = 700;
-    
+    //积分类型
+    public static final int BBS_TOPIC_SCORE = 10;
+    public static final int BBS_POST_SCORE = 3;
+    public static final int BBS_REPLAY_SCORE = 3;
+
 }
